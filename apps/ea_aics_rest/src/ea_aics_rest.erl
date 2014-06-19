71c83d34df11fba12e15298aac551f9a9f7d1078
bhushanvadgave <bhushan.vadgave@rediffmail.com>
2014-06-19 18:36:24 +0530

    Changed this module to add new dispach methode.

--- apps/ea_aics_rest/src/ea_aics_rest.erl
+++ apps/ea_aics_rest/src/ea_aics_rest.erl
@@ -7,10 +7,6 @@
 
 -module(ea_aics_rest).
 
--ifdef(TEST).
--include_lib("eqc/include/eqc.hrl").
--include_lib("eunit/include/eunit.hrl").
--endif.
 
 -export([out/1]).
 
@@ -46,6 +42,8 @@ out(#arg{} = WebArg) ->
 
 dispatch(Method, WebArg, ["ancillaries" | _RestPath] = Path) ->
     ea_aics_rest_ancillaries:process(Method, WebArg, Path);
+dispatch(Method, WebArg, ["book-ancillaries" | _RestPath] = Path) ->
+    ea_aics_rest_ancillaries:process(Method, WebArg, Path);
 dispatch(Method, WebArg, ["flights", _Uri_FlightId, "allocated-ancillaries" | _RestPath] = Path) ->
     ea_aics_rest_flight_allocated_ancillaries:process(Method, WebArg, Path);
 dispatch(Method, WebArg, ["flights", _Uri_FlightId, "ancillary-bookings" | _RestPath] = Path) ->
@@ -60,79 +58,4 @@ post_process(Result) ->
 %% ===================================================================
 %%  Tests
 %% ===================================================================
-
--ifdef(TEST).
-
-module_test_() ->
-
-    HttpRequestMethod = 'GET',
-    HttpRequestContentBody = ?HTTP_CONTENT_BODY_EMPTY,
-    HttpRequestHeaders = [],
-
-    {foreach,
-     fun()  ->
-        ok = meck:new(ea_aics_rest_ancillaries, [non_strict, {stub_all, ok}]),
-        ok = meck:new(ea_aics_rest_flight_allocated_ancillaries, [non_strict, {stub_all, ok}]),
-        ok = meck:new(ea_aics_rest_flight_ancillary_bookings, [non_strict, {stub_all, ok}])
-     end,
-     fun(_) ->
-        ?assert(meck:validate(ea_aics_rest_ancillaries)),
-        ?assert(meck:validate(ea_aics_rest_flight_allocated_ancillaries)),
-        ?assert(meck:validate(ea_aics_rest_flight_ancillary_bookings)),
-        ok = meck:unload(ea_aics_rest_ancillaries),
-        ok = meck:unload(ea_aics_rest_flight_allocated_ancillaries),
-        ok = meck:unload(ea_aics_rest_flight_ancillary_bookings)
-     end,
-     [
-        {"request dispatch",
-            [
-                fun() ->
-                    WebArg = ea_aics_rest_test:web_arg(HttpRequestMethod, HttpRequestContentBody, HttpRequestHeaders),
-                    Path = ["ancillaries"],
-                    _Response = dispatch(HttpRequestMethod, WebArg, Path),
-                    ok = meck:wait(1, ea_aics_rest_ancillaries, process, '_', 1000)
-                end
-            ]
-        },
-        {"request dispatch",
-            [
-                fun() ->
-                    WebArg = ea_aics_rest_test:web_arg(HttpRequestMethod, HttpRequestContentBody, HttpRequestHeaders),
-                    Path = ["flights", "111", "allocated-ancillaries", "111"],
-                    _Response = dispatch(HttpRequestMethod, WebArg, Path),
-                    ok = meck:wait(1, ea_aics_rest_flight_allocated_ancillaries, process, '_', 1000)
-                end
-            ]
-        },
-        {"request dispatch",
-            [
-                fun() ->
-                    WebArg = ea_aics_rest_test:web_arg(HttpRequestMethod, HttpRequestContentBody, HttpRequestHeaders),
-                    Path = ["flights", "111", "ancillary-bookings"],
-                    _Response = dispatch(HttpRequestMethod, WebArg, Path),
-                    ok = meck:wait(1, ea_aics_rest_flight_ancillary_bookings, process, '_', 1000)
-                end
-            ]
-        },
-        {"request dispatch",
-            [
-                fun() ->
-                    WebArg = ea_aics_rest_test:web_arg(HttpRequestMethod, HttpRequestContentBody, HttpRequestHeaders),
-                    Path = ["flights", "foobar"],
-                    ?assertMatch([{status, ?HTTP_404}], dispatch(HttpRequestMethod, WebArg, Path))
-                end
-            ]
-        },
-        {"request dispatch",
-            [
-                fun() ->
-                    WebArg = ea_aics_rest_test:web_arg(HttpRequestMethod, HttpRequestContentBody, HttpRequestHeaders),
-                    Path = ["flights", "111"],
-                    ?assertMatch([{status, ?HTTP_404}], dispatch(HttpRequestMethod, WebArg, Path))
-                end
-            ]
-        }
-     ]
-    }.
-
--endif.
+         
