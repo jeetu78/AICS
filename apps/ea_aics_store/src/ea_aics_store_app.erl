aebda10a206e8fc2675c66e2f4b64db8478a2245
bhushanvadgave <bhushan.vadgave@rediffmail.com>
2014-06-25 14:52:04 +0530

    updated to get DB config from resource file

--- apps/ea_aics_store/src/ea_aics_store_app.erl
+++ apps/ea_aics_store/src/ea_aics_store_app.erl
@@ -34,14 +34,6 @@
 
 start(_Type, _StartArgs) ->
     {ok, SupervisorPid} = supervisor:start_link({?SCOPE, ?MODULE}, ?MODULE, []),
-%%     PoolConfig = [{name, memsql},
-%%                   {max_count, 1},
-%%                   {init_count, 1},
-%%                   {start_mfa,
-%%                     {mysql_conn,
-%%                      start_link,
-%%                      ["127.0.0.1", 3306, "root", "", "AICS", fun(_, _, _, _) -> ok end, utf8, memsql]}}],
-%%     {ok, _PoolPid} = pooler:new_pool(PoolConfig),
     {ok, SupervisorPid}.
 
 stop(_State) ->
@@ -62,7 +54,13 @@ init([]) ->
 pooler_supervisor_child_spec() ->
 	DBConfig=getDBConfig(),
     {conn_sup, {connectionServer, start_server, [DBConfig]},
-        permanent, 5000, worker, [connectionServer]}.
+        permanent, infinity, worker, [connectionServer]}.
 
 getDBConfig() ->
-	#dbConfig{host="192.168.1.23",port=3306,database="AICS_NEW",user="root",password=""}.
+        {ok,Host}=application:get_env(host),
+        {ok,Port}=application:get_env(port),
+        {ok,Database}=application:get_env(database),
+        {ok,User}=application:get_env(user),
+        {ok,Password}=application:get_env(password),
+        #dbConfig{host=Host,port=Port,database=Database,user=User,password=Password}.
+%%  	#dbConfig{host="192.168.1.23",port=3306,database="AICS_NEW",user="root",password=""}.
